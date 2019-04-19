7577f956e95e383ef82a977d19a34bdc3c0c8485
Ceferino Jose II <cefjoeii@gmail.com>
2019-04-19 18:20:17 +0800

    Add enterprise value formula

--- finance.py
+++ finance.py
@@ -7,3 +7,11 @@
 # t = the number of years
 def get_future_value(pv, i, n, t):
   return pv * ((1 + (i/n)) ** t)
+
+# See: https://www.investopedia.com/terms/e/enterprisevalue.asp
+# EV = enterprise value
+# MC = market capitalization
+# d = total debt
+# c = cash and cash equivalent
+def get_enterprise_value(mc, d, c):
+  return mc + d - c
